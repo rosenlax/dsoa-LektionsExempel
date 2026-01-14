043a2339b9d427649ea27a727c904e280f7bd40b
rosenlax <rosenlax@arcada.fi>
2026-01-14 15:41:13 +0200

    Lektion1, car class with setters

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
        public static void main(String[] args) {
-              System.out.println("hello");
+           System.out.println("hello");
+
+           Car car = new Car("SAAB");
+           Car car2 = new Car("Ford");
+
+           System.out.println(car.getName());
+           car.setName("Volvo");
+           System.out.println(car.getName());
+           System.out.println(car2.getName());
        }
 
 }
